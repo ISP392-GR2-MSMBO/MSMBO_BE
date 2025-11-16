d8837fe0b9abb1f2132fe3ad71d3542b392c2b64
myanh1208 <lehoangmyanh1208@gmail.com>
2025-11-16 21:56:01 +0700

    Update OpenApiConfig.java

--- src/main/java/com/example/ticket_booking_system/auth/OpenApiConfig.java
+++ src/main/java/com/example/ticket_booking_system/auth/OpenApiConfig.java
@@ -1,20 +1,20 @@
-//package com.example.ticket_booking_system.auth;
-//
-//import io.swagger.v3.oas.models.OpenAPI;
-//import io.swagger.v3.oas.models.servers.Server;
-//import org.springframework.context.annotation.Bean;
-//import org.springframework.context.annotation.Configuration;
-//
-//import java.util.List;
-//
-//@Configuration
-//public class OpenApiConfig {
-//
-//    @Bean
-//    public OpenAPI customOpenAPI() {
-//        return new OpenAPI()
-//                .servers(List.of(
-//                        new Server().url("https://api-movie6868.purintech.id.vn")
-//                ));
-//    }
-//}
+package com.example.ticket_booking_system.auth;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.servers.Server;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.util.List;
+
+@Configuration
+public class OpenApiConfig {
+
+    @Bean
+    public OpenAPI customOpenAPI() {
+        return new OpenAPI()
+                .servers(List.of(
+                        new Server().url("https://api-movie6868.purintech.id.vn")
+                ));
+    }
+}
